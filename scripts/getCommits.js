f35025f945d864259a37d16e1ad0f0603926501e
skratchdot <git@skratchdot.com>
2016-06-24 08:52:37 -0400

    including all commits

--- scripts/getCommits.js
+++ scripts/getCommits.js
@@ -3,7 +3,9 @@ const childProcess = require('child_process');
 const crypto = require('crypto');
 const execSync = childProcess.execSync;
 const fs = require('fs');
+const ProgressBar = require('progress');
 const gitDir = `${__dirname}/../web-audio-api/`;
+const errors = [];
 
 const gitLog = function (format, path, max) {
   return execSync(`git log --pretty=format:"${format}" -n ${max} ${path}`, {
@@ -11,9 +13,38 @@ const gitLog = function (format, path, max) {
   }).toString().trim();
 };
 
-const commitsAsText = gitLog('%H,%ct', 'index.html', 10000);
+const gitShow = function (tree, path) {
+  return execSync(`git show ${tree}:${path}`, {
+    cwd: gitDir,
+    stdio: ['pipe', 'pipe', 'ignore']
+  }).toString();
+};
 
-const commits = commitsAsText.split('\n').map((line) => {
+const writeArchives = function (tree, path) {
+  let success = false;
+  try {
+    const contents = gitShow(tree, path);
+    const filePrefix = `./data/archives/${tree}`;
+    fs.writeFileSync(`${filePrefix}.html`, contents, 'utf-8');
+    success = true;
+  } catch (e) {
+    errors.push({
+      tree: tree,
+      msg: e.message
+    });
+  }
+  return success;
+};
+
+const commitsAsText = gitLog('%H,%ct', '', 10000);
+const lines = commitsAsText.split('\n');
+const format = '[:bar] :hash (:current of :total) :percent :elapseds :etas';
+const bar = new ProgressBar(format, {
+  total: lines.length,
+  width: 15
+});
+
+const commits = lines.map((line) => {
   const items = line.split(',');
   const hash = items[0];
   const time = parseInt(items[1], 10);
@@ -25,11 +56,20 @@ const commits = commitsAsText.split('\n').map((line) => {
   const md5Hash = crypto.createHash('md5');
   md5Hash.update(email.trim().toLowerCase());
   const md5 = md5Hash.digest('hex');
-  return { hash, time, name, email, md5, subject, body };
+  let index = null;
+  if (writeArchives(hash, 'index.html')) {
+    index = 'index.html';
+  } else if (writeArchives(hash, 'webaudio/specification.html')) {
+    index = 'webaudio/specification.html';
+  }
+  bar.tick(1, { hash: hash.slice(0, 6) });
+  return { hash, time, name, email, md5, subject, body, index };
 });
 
 const commitsStringified = JSON.stringify(commits, null, '\t');
+const errorsStringified = JSON.stringify(errors, null, '\t');
 
 // write files
 fs.writeFileSync('./data/commits.txt', commitsAsText, 'utf-8');
 fs.writeFileSync('./data/commits.json', commitsStringified, 'utf-8');
+fs.writeFileSync('./data/errors.json', errorsStringified, 'utf-8');
